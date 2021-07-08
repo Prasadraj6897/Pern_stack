df363672b499478e74e2b66b80cc2607e41565a2
Prasadraj6897 <prasadraj32895@gmail.com>
2021-07-08 13:59:35 +0530

    Created insert data functionality

--- server/index.js
+++ server/index.js
@@ -11,6 +11,20 @@ app.use(express.json());
 
 //create a todo
 
+app.post("/todos", async(req, res)=>{
+    try{
+        const {description} = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1) RETURNING * ",[description]
+        );
+        res.json(newTodo.rows[0])
+    }
+    catch(error)
+    {
+        console.log(error.message)
+    }
+})
+
 app.listen(5000, ()=>{
     console.log("server running on port 5000")
 });
