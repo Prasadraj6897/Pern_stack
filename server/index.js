571a32a8be939392a1c4a7a463ba68803d310c78
Prasadraj6897 <prasadraj32895@gmail.com>
2021-07-08 13:37:08 +0530

    Created Database Schema

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const pool = require("./db")
 //middleware
 
 app.use(cors());
 app.use(express.json());
 
+//Routes
+
+//create a todo
 
 app.listen(5000, ()=>{
     console.log("server running on port 5000")
